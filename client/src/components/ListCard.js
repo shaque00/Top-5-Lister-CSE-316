d9db8d50d5bac94167e255e001cf57dfa047c3e1
Saiful Haque <saiful.haque@stonybrook.edu>
2021-11-29 16:27:46 -0500

    asdf

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -130,7 +130,7 @@ function ListCard(props) {
                         <Grid item sx={{p:1}} xs={12}><Paper style={{maxHeight:100, overflow: 'auto'}}>
                         {
                             idNamePair.items.map((item, index) => (
-                                <Typography sx={{left: "5px",p:1}}variant="h6" component="h6">
+                                <Typography sx={{left: "5px"}}variant="h5" component="h5">
                                     {index+1}. {item}
                                 </Typography>
                             ))
