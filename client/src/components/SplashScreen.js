1e0c15228398a175515827e2048eba55eefa3654
Saiful Haque <saiful.haque@stonybrook.edu>
2021-12-06 20:21:29 -0500

    2 more things

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,4 +1,9 @@
+import { Button } from "@mui/material"
+import { Box } from "@mui/system"
+import { Link } from "react-router-dom"
+
 export default function SplashScreen() {
+
     return (
         <div id="splash-screen">
             The Top 5 Lister
@@ -8,6 +13,10 @@ export default function SplashScreen() {
             <div id="dev-text">
             Here, you can share your Top 5's with everyone, and see other's as well!
             </div>
+            <Box sx={{p:1}}>
+                <Button sx={{p:1, right:"15px"}}variant="contained"> <Link to='/login/'>Login</Link> </Button>
+                <Button sx={{p:1, left:"15px"}}variant="contained"> <Link to='/register/'>register</Link> </Button>
+            </Box>
         </div>
     )
 }
